7504c690983f4a563dc658378acc1877b395ef55
cmd-khushi <kk9066920@gmail.com>
2025-01-02 23:02:30 +0530

    Create 1929.c

---
+++ 1929.c
@@ -0,0 +1,10 @@
+int* getConcatenation(int* nums, int numsSize, int* returnSize) {
+    *returnSize = 2 * numsSize;
+    int* ans = (int*)malloc(2 * numsSize * sizeof(int));
+    if (ans == NULL) return NULL;
+    for (int i = 0; i < numsSize; i++) {
+        ans[i] = nums[i];
+        ans[i + numsSize] = nums[i];
+    }
+    return ans;
+}
