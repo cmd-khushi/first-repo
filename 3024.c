f4a90fe1bbf26fb310c8619df447e58671d11fe3
cmd-khushi <kk9066920@gmail.com>
2025-01-02 23:06:01 +0530

    Create 3024.c

---
+++ 3024.c
@@ -0,0 +1,17 @@
+char* triangleType(int* nums, int numsSize) {
+    int a = nums[0];
+    int b = nums[1];
+    int c = nums[2];
+
+    if ((a + b <= c) || (b + c <= a) || (a + c <= b)) {
+        return "none";
+    }
+
+    if (a == b && b == c) {
+        return "equilateral";
+    } else if (a == b || b == c || a == c) {
+        return "isosceles";
+    } else {
+        return "scalene";
+    }
+}
